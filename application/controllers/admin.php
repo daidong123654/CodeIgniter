be8ef4176b0597c8f38708fd39d340acf23bb191
代东 <daidong123654@gmail.com>
2012-09-05 22:03:05 +0800

    第二次提交
    简单的熟悉了框架，在上面搭建了一个简单的新闻发布平台。

---
+++ application/controllers/admin.php
@@ -0,0 +1,9 @@
+﻿<?php
+class admin extends CI_Controller
+{
+	public function __construct()
+	{ 
+		parent::__construct();
+		$this->load->model('admin_model');
+	}
+?>
---
+++ application/views/templates/header.php
@@ -0,0 +1,23 @@
+<html>
+  <head>
+    <title>Bootstrap 101 Template</title>
+    <!-- Bootstrap -->
+	<?php $this->load->helper('url'); ?>
+    <link href="<?php echo base_url();?>css/bootstrap.min.css" rel="stylesheet" type="text/css" />	
+    <link href="<?php echo base_url();?>css/bootstrap.css" rel="stylesheet" type="text/css" />	
+    <link href="<?php echo base_url();?>css/bootstrap-responsive.css" rel="stylesheet" type="text/css" />	
+    <link href="<?php echo base_url();?>css/bootstrap-responsive.min.css" rel="stylesheet" type="text/css" />	
+    <link href="<?php echo base_url();?>css/css.css" rel="stylesheet" type="text/css" />	
+	<script src="<?php echo base_url();?>js/bootstrap.js"></script>
+    <script src="<?php echo base_url();?>js/bootstrap.min.js"></script>
+	<script src="http://code.jquery.com/jquery-latest.js"></script>
+  </head>
+  <body>    
+	<h1 style="text-align:center;font-family:'隶书'">图书馆管理系统</h1>  
+<div id="wrap">	
+<p>
+  <a href="http://localhost/Library/CodeIgniter_2.1.2/index.php"><button class="btn btn-primary" type="button">Main Page</button></a>
+  <a href="http://localhost/Library/CodeIgniter_2.1.2/index.php/news"><button class="btn" type="button">News Page</button> </a>
+  <a href="http://localhost/Library/CodeIgniter_2.1.2/index.php/news/create"><button class="btn btn-success" type="button">Create News</button></a>
+</p>
+
